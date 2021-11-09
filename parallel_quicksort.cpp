5615d0c2d031c0ea0cfca2dbc6212a344be6cc54
omeiza <omeiza.olumoye@gmail.com>
2021-11-09 00:48:08 -0500

    added an if-statement

--- parallel_quicksort.cpp
+++ parallel_quicksort.cpp
@@ -11,15 +11,19 @@ using namespace std;
 int main(int argc, char *argv[]) {
 
 
-    int id, num_of_threads;
-
     cout << "Hello parallel world from all threads" << endl;  
     #pragma omp parallel
     {
+        int id, num_of_threads;
         num_of_threads = omp_get_num_threads();
         id = omp_get_thread_num();
         usleep(5000 * id);  // to avoid race condition when printing
-        cout << "Greetings from process " << id << " out of " << num_of_threads << endl;  
+        if (id == 0){
+            cout << "Greetings from process " << id << " out of " << num_of_threads << " -- I am MASTER of all" << endl;  
+        }
+        else {
+            cout << "Greetings from process " << id << " out of " << num_of_threads << endl; 
+        }
     }
     cout << "Back to the sequential world." << endl;  
 
