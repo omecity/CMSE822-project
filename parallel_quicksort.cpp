4dbb2c37d5f1344812f5fdb1a0da6e02b42d2dac
omeiza <omeiza.olumoye@gmail.com>
2021-11-10 23:09:18 -0500

    changed the process variable from id to rank

--- parallel_quicksort.cpp
+++ parallel_quicksort.cpp
@@ -8,21 +8,21 @@ using namespace std;
 // parallel_quicksort.cpp
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
 
 
     cout << "Hello parallel world from all threads" << endl;  
     #pragma omp parallel
     {
-        int id, num_of_threads;
+        int rank, num_of_threads;
         num_of_threads = omp_get_num_threads();
-        id = omp_get_thread_num();
-        usleep(5000 * id);  // to avoid race condition when printing
-        if (id == 0){
-            cout << "Greetings from process " << id << " out of " << num_of_threads << " -- I am MASTER of all" << endl;  
+        rank = omp_get_thread_num();
+        usleep(5000 * rank);  // to avoid race condition when printing
+        if (rank == 0){
+            cout << "Greetings from process " << rank << " out of " << num_of_threads << " -- I am MASTER of all" << endl;  
         }
         else {
-            cout << "Greetings from process " << id << " out of " << num_of_threads << endl; 
+            cout << "Greetings from process " << rank << " out of " << num_of_threads << endl; 
         }
     }
     cout << "Back to the sequential world." << endl;  
