d26c259c1efc0b5e8d32ba66333c9378ca778607
omeiza <omeiza.olumoye@gmail.com>
2021-11-11 22:54:59 -0500

    created the method to return the index of the array

---
+++ pivot_last.cpp
@@ -0,0 +1,13 @@
+
+
+double getLastElement(int arr[]){
+  int i, sum = 0;       
+  double avg;          
+
+   for (i = 0; i < size; ++i) {
+      sum += arr[i];
+   }
+   avg = double(sum) / size;
+
+   return avg;
+}
