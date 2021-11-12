30ae514f20687ea5b6afbf092efc535eacf6989e
omeiza <omeiza.olumoye@gmail.com>
2021-11-11 22:54:48 -0500

    created the method to return the index of the array

---
+++ pivot_first.cpp
@@ -0,0 +1,13 @@
+
+
+double getFirstElement(int arr[]){
+  int i, sum = 0;       
+  double avg;          
+
+   for (i = 0; i < size; ++i) {
+      sum += arr[i];
+   }
+   avg = double(sum) / size;
+
+   return avg;
+}
