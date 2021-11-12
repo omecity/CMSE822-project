1217f3b259e2da94a1a5f2c12db17603dc77a5b5
omeiza <omeiza.olumoye@gmail.com>
2021-11-11 22:55:14 -0500

    created the method to return the index of the array

---
+++ pivot_prefix.cpp
@@ -0,0 +1,13 @@
+
+
+double getPrefixElement(int arr[]){
+  int i, sum = 0;       
+  double avg;          
+
+   for (i = 0; i < size; ++i) {
+      sum += arr[i];
+   }
+   avg = double(sum) / size;
+
+   return avg;
+}
