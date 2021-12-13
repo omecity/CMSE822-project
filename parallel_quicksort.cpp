8063b93572710aeba584a64f19b066c059a34287
omeiza <omeiza.olumoye@gmail.com>
2021-12-13 18:28:31 -0500

    taking some more readings

--- parallel_quicksort.cpp
+++ parallel_quicksort.cpp
@@ -35,17 +35,17 @@ int main(int argc, char *argv[]) {
     // option = atoi(argv[1]);
     // arr_size = atoi(argv[2]);
 
-    int arr[8] = {5, 10, 2, 4, 3, 5, 8, 1};
+    int arr[11] = {5, 10, 2, 4, 3, 5, 8, 1, 12, 23, 15};
     // int arr[arr_size];
     int i;
 
-    arr_size = 8;
+    arr_size = 11;
     // fillArray(arr, arr_size);
     printArray(arr, arr_size);
 
-    quicksort(arr, 0, 7);
+    quicksort(arr, 0, 10);
 
-    cout << "===================================== " << endl;
+    cout << " ===================================== " << endl;
     printArray(arr, arr_size);
     // testing the values 
     /*
@@ -60,22 +60,22 @@ int main(int argc, char *argv[]) {
     */
 
     cout << " " << endl;
-    cout << "Hello parallel world from all threads" << endl;  
+    cout << "Hello parallel world from all threads" << endl; 
+
+    // int max_threads = omp_set_num_threads(); 
     
-    
-    /*
     start = omp_get_wtime();
     #pragma omp parallel
     {
-        int rank, num_of_threads;
-        num_of_threads = omp_get_num_threads();
+        int rank, nthreads;
+        nthreads = omp_get_num_threads();
         rank = omp_get_thread_num();
         usleep(5000 * rank);        // to avoid race condition when printing
         if (rank == 0){
-            cout << "Greetings from process " << rank << " out of " << num_of_threads << " -- I am MASTER of all " << endl;  
+            cout << "Greetings from process " << rank << " out of " << nthreads << " -- I am MASTER of all " << endl;  
         }
         else {
-            cout << "Greetings from process " << rank << " out of " << num_of_threads << endl; 
+            cout << "Greetings from process " << rank << " out of " << nthreads << endl; 
         }
     }
     end = omp_get_wtime();
@@ -84,7 +84,6 @@ int main(int argc, char *argv[]) {
     // //print out the resulting elapsed time
     cout << "Time for parallel computation region: "<< time_elapsed << " seconds." << endl;
     cout << "Back to the sequential world." << endl;  
-    */
 
     // int option, index, arr_size;
     cout << "  " << endl;    
@@ -110,17 +109,8 @@ int main(int argc, char *argv[]) {
     // else{
     //     index = getPrefixElement(arr);
     // }
-
-
   return 0;
 }
-
-// print the elements of the array
-// std::cout << "arr: ";
-// std::copy(arr.begin(),arr.end(), std::ostream_iterator<int>(std::cout, ","));
-// cout << "This is the first element" << endl;
-// cout << "This is the first element" << endl;
-// cout << "This is the first element" << endl;
 
 int getFirstIndex(){      
    return 0;
@@ -148,81 +138,25 @@ void printArray(int arr[], int arr_size){
 
 void quicksort(int arr[], int left, int right){
     if (left < right){
-        // int pivot = arr[left+right/2];
-        // int idx = partition(arr,left,right,pivot);
         int idx = partition(arr,left,right);
-
         // sort the sub-arrays recursively
         quicksort(arr, left, idx-1);
         quicksort(arr, idx+1, right);
-        // for (int i = 0; i < arr_size; i++)
-            // cout << "The elements of the array is/are " << arr[i] << endl;
     }
 }
-
-// int partition(int arr[], int left, int right){
-//     int pivot = arr[right];
-//     while (left < right){
-//         while (arr[left] < pivot){
-//             left++;
-//         }
-//          while (arr[right] > pivot){
-//             right--;
-//         }
-//         if (left <= right){
-//             swap(arr[left],arr[right]);
-//             left++;
-//             right--;
-//         }
-//     }
-//     return left;
-// }
-
-// // partition the array using last element as pivot
-// int partition (int arr[], int low, int high) 
-// { 
-//     int pivot = arr[high];    // pivot 
-//     int i = (low - 1);      
-//     for (int j = low; j <= high- 1; j++) 
-//     { 
-//         //if current element is smaller than pivot, increment the low element
-//         //swap elements at i and j
-//         if (arr[j] <= pivot) 
-//         { 
-//             i++;    // increment index of smaller element 
-//             swap(arr, i, j); 
-//         } 
-//     } 
-//     swap(arr,i,high); 
-//     return (i + 1); 
-// } 
-
-// public int Partition(int a[], int low, int high) {
-//    int pivot, index, i;
-//    index = low;
-//    pivot = high;
-//    for(i=low; i < high; i++) {
-//       if(a[i] < a[pivot]) {
-//          swap(&a[i], &a[index]);
-//          index++;
-//       }
-//    }
-//    swap(a[pivot], &a[index]);
-//    return index;
-// }
 
 int partition(int arr[], int start, int end) {
     int i = start + 1;
     int pivot = arr[start] ;            
-    for(int j = start + 1; j <= end ; j++ )  {
+    for(int j = start + 1; j <= end; j++)  {
         if (arr[j] < pivot) {
                 swap (arr,i,j);
             i = i + 1;
         }
-   }
-   //put the pivot element in its proper place.
-   swap (arr,start,i-1) ;  //put the pivot element in its proper place.
-   return i-1;                      //return the position of the pivot
+    }
+   // put the pivot element in its proper place.
+   swap (arr,start,i-1);
+   return i-1;                      
 }
 
 void swap(int arr[], int left, int right){
